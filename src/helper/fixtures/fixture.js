c521efc7afb4afdd146d233fad062969a38c4c2e
YuhnovRoman <YuhnovRoman@users.noreply.github.com>
2025-07-02 15:45:53 +0000

    deploy: e542e33d07dd14f8d8e781f5d9c100a67ec35aba

--- src/helper/fixtures/fixture.js
+++
@@ -1,9 +0,0 @@
-import { test as base } from '@playwright/test';
-import { App } from '../../pages/index';
-
-export const test = base.extend({
-  app: async ({ page }, use) => {
-    let app = new App(page);
-    await use(app);
-  },
-});
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./app.page";
-export * from "./auth.page";
-export * from "./main.page";
-export * from "./article.page";
-export * from "./author.page";
